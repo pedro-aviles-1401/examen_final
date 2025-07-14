927b48bb58bc85ac3527d80c4e6e8c0f9cd871a8
pedro-aviles-1401 <pe.aviles@duocuc.cl>
2025-07-14 16:22:40 -0400

    se elimino una lista y se agrego otra

--- examen final.py
+++ examen final.py
@@ -1,12 +1,4 @@
-#producto={"model":["marca","pantalla","RAM","disco","GB de DD","procesador ", "video"]}
-productos = {'8D475H': ['HP', 15.6, '8GB', 'DD', '1T', 'Intel Core i5', 'Nvidia GTX1050'],
-                      '2175HD': ['Acer', 14, '4GB', 'SSD', '512GB', 'Intel Core i5', 'Nvidia GTX1050'],
-                      'JjfFHD': ['Asus', 14, '16GB', 'SSD', '256GB', 'Intel Core i7', 'Nvidia RTX2080Ti'],
-                     'fgdxFHD': ['HP', 15.6, '8GB', 'DD', '1T', 'Intel Core i3', 'integrada'],
-                     'GF75HD': ['Asus', 15.6, '8GB', 'DD', '1T', 'Intel Core i7', 'Nvidia GTX1050'],
-                     '123FHD': ['Acer', 14, '6GB', 'DD', '1T', 'AMD Ryzen 5', 'integrada'],
-                     '342FHD': ['Acer', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 7', 'Nvidia GTX1050'],
-                     'UWU131HD': ['Dell', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 3', 'Nvidia GTX1050'],}
+tienda_de_computadores={}
 #stoc={"modelo":["precio,stock"]}
 stock = {'8475HD': [387990,10], '2175HD': [327990,4], 'JjfFHD': [424990,1],
               'fgdxFHD': [664990,21], '123FHD': [290890,32], '342FHD': [444990,7],
